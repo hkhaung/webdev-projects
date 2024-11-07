25d155fdab1aea6cc09045782af5fc8c98568f5d
hkhaung <henrykhaung22@gmail.com>
2024-11-06 17:29:50 -0800

    partial game logic implemented, have bugs with lives
    selecting wrong cell doesn't reset timer and grid
    
    continue implementing game logic

--- simple-memory-game/simple-memory-game/src/components/Board/Board.jsx
+++ simple-memory-game/simple-memory-game/src/components/Board/Board.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState, useMemo, useRef } from 'react';
 import './Board.css';
 
 function TimerBar({ isVisible=false }) {
@@ -11,24 +11,37 @@ function TimerBar({ isVisible=false }) {
   )
 }
 
-function Card( { textNumber='', isVisible=true } ) {
+function Card( { text, maxNumber, isVisible=true, prevCardFlipped=null, setBoardVisible=null, setLives=null } ) {
   const [isFlipped, setIsFlipped] = useState(false);
 
   function handleFlip() {
     setIsFlipped((prev) => !prev);
+
+    if (prevCardFlipped.current === null && text === 1 || (prevCardFlipped.current + 1 === text)) {
+      prevCardFlipped.current = text;
+      // if (prevCardFlipped.current === maxNumber) {
+      //   setBoardVisible(false);
+      // }
+    } else {
+      setLives((prevLives) => (prevLives > 0 ? prevLives - 1 : 0));
+      setBoardVisible(false);
+      prevCardFlipped.current = null;
+    }
+    
+    return prevCardFlipped;
   }
 
   return (
     <>
     {isVisible && (
       <div className='card'>
-        {textNumber}
+        {text}
       </div>
     )}
 
     {!isVisible && (
       <div className='card' onClick={handleFlip}>
-        <div className={`${isFlipped ? 'visible' : 'hidden'}`}>{textNumber}</div>
+        <div className={`${isFlipped ? 'visible' : 'hidden'}`}>{text}</div>
       </div>
     )}
     </>
@@ -38,6 +51,7 @@ function Card( { textNumber='', isVisible=true } ) {
 function Board({ maxNumber, setWinLose }) {
   function handleStart() {
     setIsFlipping(false);
+    setBoardVisible(true);
     setVisibleTimer(true);
     setGameStarted(true);
     setShowAllCards(true);
@@ -49,10 +63,18 @@ function Board({ maxNumber, setWinLose }) {
     clearInterval();
   }
 
+  function handleLives() {
+    setBoardVisible(true);
+    setVisibleTimer(true);
+    setShowAllCards(true);
+    setGridArray(initialValues.gridArray);
+    setNumberIndices(initialValues.numberIndices);
+  }
+
   // Generate cardsArray and randomly populate gridArray once using useMemo
   // only recompute if maxNumber or totalCells change
   const totalCells = 36;
-  const { gridArray, numberIndices } = useMemo(() => {
+  const initialValues = useMemo(() => {
     const cardsArray = Array.from({ length: maxNumber }, (_, i) => i + 1);
     const gridArray = Array.from({ length: totalCells }, () => null);
     const numberIndices = []; // Store indices where numbers are placed to be used for visibility
@@ -70,8 +92,8 @@ function Board({ maxNumber, setWinLose }) {
     return { gridArray, numberIndices };
   }, [maxNumber, totalCells]);
 
-
-  const [gameStarted, setGameStarted] = useState(false);
+  const [gridArray, setGridArray] = useState(initialValues.gridArray);
+  const [numberIndices, setNumberIndices] = useState(initialValues.numberIndices);
   
   // visual board + interval
   const [visibleCard, setVisibleCard] = useState(0);
@@ -87,6 +109,7 @@ function Board({ maxNumber, setWinLose }) {
   }, [isFlipping, numberIndices.length]);
 
   // timer
+  const prevCardFlipped = useRef(null); // useRef used to prevent double rerenders
   const [visibleTimer, setVisibleTimer] = useState(false);
   useEffect(() => {
     if (gameStarted) {
@@ -97,14 +120,18 @@ function Board({ maxNumber, setWinLose }) {
   });
 
   // game started
-  const [showAllCards, setShowAllCards] = useState(false)
+  const [gameStarted, setGameStarted] = useState(false);
+  const [lives, setLives] = useState(3);
+  const [boardVisible, setBoardVisible] = useState(false);
+  const [showAllCards, setShowAllCards] = useState(false);
   useEffect(() => {
     if (showAllCards) {
       setTimeout(() => {
         setShowAllCards(false);
       }, 10000);
     }
-  })
+  });
+
 
   return (
     <>
@@ -113,9 +140,7 @@ function Board({ maxNumber, setWinLose }) {
           <div className='grid-container hover-enabled'>
             {gridArray.map((value, index) => (
               <div key={index} className={`grid-item ${numberIndices[visibleCard] === index ? 'visible' : 'hidden'}`}>
-                <Card
-                  textNumber={value ? value.toString() : ''}
-                />
+                <Card text={value} maxNumber={maxNumber} />
               </div>
             ))}
             <button className='start-btn' onClick={handleStart}>Start</button>
@@ -125,22 +150,40 @@ function Board({ maxNumber, setWinLose }) {
 
       {gameStarted && (
         <>
-          <TimerBar isVisible={visibleTimer} />
+          {boardVisible && (
+            <>
+              <TimerBar isVisible={visibleTimer} />
 
-          <div className='board'>
-            <div className='grid-container'>
-              {gridArray.map((value, index) => (
-                <>
-                  <div key={index} className={`grid-item`}>                    
-                    <Card
-                      textNumber={value ? value.toString() : ''}
-                      isVisible={showAllCards}
-                    />
-                  </div>
-                </>
-              ))}
+              <div className='board'>
+                <div className='grid-container'>
+                  {gridArray.map((value, index) => (
+                    <div key={index} className={`grid-item`}>                    
+                      <Card
+                        text={value}
+                        maxNumber={maxNumber}
+                        isVisible={showAllCards}
+                        prevCardFlipped={prevCardFlipped}
+                        setBoardVisible={setBoardVisible}
+                        setLives={setLives}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
+
+          {!boardVisible && (
+            <div className='lives-container'>
+              <div>Lives left: {lives}</div>
+              {lives > 0 && (
+                <button onClick={handleLives}>Continue</button>
+              )}
+              {lives === 0 && (
+                <button>Restart</button>
+              )}
             </div>
-          </div>
+          )}
         </>
       )}
     </>
