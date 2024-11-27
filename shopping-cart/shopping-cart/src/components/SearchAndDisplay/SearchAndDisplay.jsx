a1b6adbdabff2b7c12900844aafd22863a046597
Henry Khaung <henrykhaung22@gmail.com>
2024-11-26 23:28:23 -0800

    search and display template done

--- shopping-cart/shopping-cart/src/components/SearchAndDisplay/SearchAndDisplay.jsx
+++ shopping-cart/shopping-cart/src/components/SearchAndDisplay/SearchAndDisplay.jsx
@@ -2,18 +2,19 @@
 // search bar to search for products
 // card display to show products
 
-function SearchBar() {
+function Search( {numProducts=50} ) {
   return (
     <>
       <div className="searchbar-container">
         <div
-          className="grid grid-cols-3 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 border border-amber-500 p-4">
-          <div className="border border-red-500 min-w-[20rem]">Products</div>
-          <form className="col-span-2 border border-red-500">
+            className="grid grid-cols-3 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4">
+          <div
+              className="place-content-center min-w-[20rem] text-2xl">Search results {numProducts > 0 ? `(${numProducts})` : ''}</div>
+          <form className="w-full col-span-2">
             <input
-              type="text"
-              placeholder="Search for items in store here..."
-              className="min-w-[375px] sm:w-[500px] p-2 border border-gray-300 rounded-md focus:border-blue-500"/>
+                type="text"
+                placeholder="Search for items in store here..."
+                className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500"/>
           </form>
         </div>
       </div>
@@ -24,7 +25,7 @@ function SearchBar() {
 function Card({imgSrc, title = "Title", description = "description", category = "category"}) {
   return (
     <>
-      <div className="card-container border rounded-lg shadow-md pb-8 min-w-[20rem] max-w-[20rem]">
+      <div className="card-container border rounded-lg shadow-md pb-8">
         <div className="border-amber-500">
           <img src={imgSrc} alt={`${title} image`} className="w-full min-h-[10rem] object-cover rounded-t-lg" />
         </div>
@@ -41,7 +42,7 @@ function Card({imgSrc, title = "Title", description = "description", category = 
 function CardsDisplay() {
   return (
     <>
-      <div className="display-container border border-red-500">
+      <div className="display-container">
         <div className="cards-container grid grid-cols-3 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4">
           <Card imgSrc="https://placehold.co/600x400" description="Lorem ipsum odor amet, consectetuer adipiscing elit. Metus natoque tempor lobortis vivamus suspendisse nisl; etiam ultricies ad. Risus est ex consequat libero enim dui venenatis interdum." />
           <Card imgSrc="https://placehold.co/600x400" />
@@ -61,8 +62,8 @@ function CardsDisplay() {
 function SearchAndDisplay() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center gap-8 p-20">
-        <SearchBar/>
+      <div className="flex flex-col justify-center items-center gap-1 p-20">
+        <Search />
         <CardsDisplay />
       </div>
     </>
