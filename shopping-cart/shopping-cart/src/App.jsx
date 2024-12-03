4970f3f2b37fc1c6cbaaf39689c06cfdb565af26
Henry Khaung <henrykhaung22@gmail.com>
2024-12-03 00:16:56 -0800

    add navbar

--- shopping-cart/shopping-cart/src/App.jsx
+++ shopping-cart/shopping-cart/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from "./components/Navbar/Navbar.jsx";
 import SearchAndDisplay from "./components/SearchAndDisplay/SearchAndDisplay.jsx";
 
 function App() {
   return (
     <>
       <div>
+        <Navbar />
         <SearchAndDisplay />
       </div>
     </>
---
+++ shopping-cart/shopping-cart/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar bg-white border-gray-200">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="" className="flex items-center space-x-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="54" height="41" fill="none" viewBox="0 0 54 41">
+              <path fill="#2A2E4E" d="M54 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"></path>
+              <path fill="#2A2E4E" fillRule="evenodd"
+                    d="M13.75 40.794C6.156 40.794 0 34.638 0 27.044V1h5v26.044a8.75 8.75 0 0 0 8.75 8.75c4.893 0 8.75-3.771 8.75-8.544h5v7.5a1.25 1.25 0 0 0 2.5 0v-8.875a6.25 6.25 0 0 1-7.5-6.125V7.25a6.25 6.25 0 1 1 12.5 0v27.5a1.25 1.25 0 1 0 2.5 0V7.25a6.25 6.25 0 1 1 12.5 0v27.5a6.25 6.25 0 0 1-10 5A6.222 6.222 0 0 1 36.25 41a6.222 6.222 0 0 1-3.75-1.25 6.251 6.251 0 0 1-9.466-2.47c-2.456 2.197-5.723 3.514-9.284 3.514Zm30-4.794c-.69 0-1.25-.56-1.25-1.25V7.25a1.25 1.25 0 1 1 2.5 0v27.5c0 .69-.56 1.25-1.25 1.25ZM30 19.75a1.25 1.25 0 0 1-2.5 0V7.25a1.25 1.25 0 1 1 2.5 0v12.5Z"
+                    clipRule="evenodd"></path>
+              <path fill="#2A2E4E" fillRule="evenodd"
+                    d="M7.5 27.25a6.25 6.25 0 1 0 12.5 0v-20a6.25 6.25 0 1 0-12.5 0v20Zm6.25 1.25c-.69 0-1.25-.56-1.25-1.25v-20a1.25 1.25 0 1 1 2.5 0v20c0 .69-.56 1.25-1.25 1.25Z"
+                    clipRule="evenodd"></path>
+            </svg>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap">Search and Cart</span>
+          </a>
+
+          <a href="#" className="flex items-center text-gray-600">
+            <svg xmlns="http://www.w3.org/2000/svg" width="54" height="32" fill="currentColor" className="bi bi-cart hover:fill-current hover:text-black transition duration-300 ease-in-out"
+                 viewBox="0 0 16 16">
+              <path
+                d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+            </svg>
+          </a>
+
+
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar;
