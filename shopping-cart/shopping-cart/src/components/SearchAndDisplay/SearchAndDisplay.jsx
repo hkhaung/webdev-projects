26ff5243d86ea6251a2e1b732c4c82c56d254d96
Henry Khaung <henrykhaung22@gmail.com>
2024-12-02 23:30:21 -0800

    add buttons for cards
    better styling for search bar
    
    min-height adjustments for smaller screens

--- shopping-cart/shopping-cart/src/components/SearchAndDisplay/SearchAndDisplay.jsx
+++ shopping-cart/shopping-cart/src/components/SearchAndDisplay/SearchAndDisplay.jsx
@@ -14,7 +14,7 @@ function Search( {numProducts=50} ) {
             <input
               type="text"
               placeholder="Search for items in store here..."
-              className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500"/>
+              className="w-full p-2 border border-gray-300 rounded-md text-slate-700 placeholder:text-slate-400 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"/>
           </form>
         </div>
       </div>
@@ -25,13 +25,27 @@ function Search( {numProducts=50} ) {
 function Card({imgSrc, title = "Title", description = "description", category = "category"}) {
   return (
     <>
-      <div className="card-container border rounded-lg shadow-md pb-8 w-full max-w-[20rem]">
-        <img src={imgSrc} alt={`${title} image`} className="w-full min-h-[10rem] object-cover rounded-t-lg" />
-        <div className="info mt-5 px-4 min-h-[12rem] max-h-[12rem]">
-          <div className="title font-medium text-xl">{title}</div>
-          <div className="description mt-1 text-gray-600 line-clamp-5 min-h-[7.5rem]">{description}</div>
-          <div className="category mt-8 text-sm text-gray-600 italic">{category}</div>
+      <div
+          className="card-container border rounded-lg shadow-md pb-8 w-full max-w-[20rem] group/default hover:bg-slate-100">
+        <div className="group-hover/default:hidden">
+          <img src={imgSrc} alt={`${title} image`} className="w-full min-h-[10rem] object-cover rounded-t-lg"/>
+          <div className="info mt-5 px-4 min-h-[12rem] max-h-[12rem]">
+            <div className="title font-medium text-xl">{title}</div>
+            <div className="description mt-1 text-gray-600 line-clamp-5 min-h-[7.5rem]">{description}</div>
+            <div className="category mt-8 text-sm text-gray-600 italic">{category}</div>
+          </div>
         </div>
+
+        <div className="transition-opacity duration-300 ease-in opacity-0 group-hover/default:opacity-100">
+          <div className="hidden group-hover/default:flex items-center justify-center h-full min-h-[26.5rem] md:min-h-[24.5rem]">
+            <div className="flex flex-col items-center justify-center space-y-4">
+              <button className="add-to-cart-btn w-full px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition duration-300 ease-in-out">Add to cart</button>
+              <button className="remove-from-cart-btn w-full px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 transition duration-300 ease-in-out">Remove from cart</button>
+            </div>
+          </div>
+        </div>
+
+
       </div>
     </>
   )
@@ -39,10 +53,11 @@ function Card({imgSrc, title = "Title", description = "description", category = 
 
 function CardsDisplay() {
   return (
-    <>
-      <div className="display-container mt-8">
-        <div className="cards-container grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
-          <Card imgSrc="https://placehold.co/600x400" description="Lorem ipsum odor amet, consectetuer adipiscing elit. Metus natoque tempor lobortis vivamus suspendisse nisl; etiam ultricies ad. Risus est ex consequat libero enim dui venenatis interdum." />
+      <>
+        <div className="display-container mt-8">
+          <div className="cards-container grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+            <Card imgSrc="https://placehold.co/600x400"
+                  description="Lorem ipsum odor amet, consectetuer adipiscing elit. Metus natoque tempor lobortis vivamus suspendisse nisl; etiam ultricies ad. Risus est ex consequat libero enim dui venenatis interdum." />
           <Card imgSrc="https://placehold.co/600x400" />
           <Card imgSrc="https://placehold.co/600x400" />
           <Card imgSrc="https://placehold.co/600x400" />
